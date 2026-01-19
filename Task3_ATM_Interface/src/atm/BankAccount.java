f2ed3743c5ad2b867b786050b2ab61378563308a
Nandeesh Gowda M S <nandeeshgowdams1713@gmail.com>
2026-01-19 14:33:17 +0530

    Added Task 3 - ATM Interface

---
+++ Task3_ATM_Interface/src/atm/BankAccount.java
@@ -0,0 +1,32 @@
+package atm;
+
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful.");
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+}
